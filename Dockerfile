b6d07f6afa8ec990b80a2483d8b294d89f188815
Z3r0xF <Z3r0xF@proton.me>
2025-10-30 20:27:04 +0100

    Delete time

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.14-slim
 WORKDIR /app
 
 ENV API_KEY=${API_KEY}
+ENV DELETE_TIME_MINUTES=${DELETE_TIME_MINUTES}
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI, Depends, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.security import APIKeyHeader
+from pydantic import BaseModel
 import threading
 import os
 import uuid
 import time
 import logging
+
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 app = FastAPI()
@@ -32,6 +34,9 @@ async def verify_api_key(api_key: str = Depends(api_key_header)):
 
 @app.post("/upload", dependencies=[Depends(verify_api_key)])
 async def upload_file( file: UploadFile = File(...)):
+
+
+
     # Generate a unique filename and respect the image format
     new_filename = f"{uuid.uuid4()}.{file.filename.split('.')[-1]}"
     file_location = os.path.join(UPLOAD_FOLDER, new_filename)
@@ -39,9 +44,16 @@ async def upload_file( file: UploadFile = File(...)):
     # Save the uploaded file
     with open(file_location, "wb") as f:
         f.write(await file.read())
-    
-    threading.Thread(target=delete_file_after_delay, args=(file_location, 5)).start()
 
+
+    #Default value is 0
+    DELETE_TIME_MINUTES = os.getenv("DELETE_TIME_MINUTES")
+
+    if int(DELETE_TIME_MINUTES) > 0:
+        logging.info(f"Deleting file: {file_location} in {DELETE_TIME_MINUTES} minutes")
+        #Convert seconds to minutes
+        DELETE_TIME_MINUTES = int(DELETE_TIME_MINUTES) * 60
+        threading.Thread(target=delete_file_after_delay, args=(file_location, DELETE_TIME_MINUTES)).start()
 
     # Generate a link to the uploaded file
     return {
